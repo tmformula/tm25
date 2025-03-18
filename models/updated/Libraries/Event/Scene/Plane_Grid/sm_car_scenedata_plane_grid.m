96e9acb3e4b49dff795f32bd3e5249bcab21cbda
Malcolm Boyes <38740057+boyesm@users.noreply.github.com>
2025-03-18 15:16:58 -0400

    Update sm_car_scenedata_plane_grid.m

--- models/updated/Libraries/Event/Scene/Plane_Grid/sm_car_scenedata_plane_grid.m
+++ models/updated/Libraries/Event/Scene/Plane_Grid/sm_car_scenedata_plane_grid.m
@@ -3,8 +3,8 @@ function scene_data = sm_car_scenedata_plane_grid
 % Copyright 2018-2024 The MathWorks, Inc.
 
 scene_data.Name = 'Plane_Grid';
-scene_data.Plane.l = 10000;           % m
-scene_data.Plane.w = 10000;           % m
+scene_data.Plane.l = 40000;           % m
+scene_data.Plane.w = 400;           % m
 scene_data.Plane.h = 0.01;          % m
 scene_data.Plane.x = 200;           % m
 scene_data.Plane.y = 0;             % m
@@ -12,7 +12,7 @@ scene_data.Plane.z = 0;             % m
 scene_data.Plane.clr = [1 1 1]*0.8; % [R G B]
 scene_data.Plane.opc = 1;           % (0-1)
 scene_data.Grid.h = 0.02;           % m
-scene_data.Grid.num_sqrs_x = 40;    % m
+scene_data.Grid.num_sqrs_x = 4000;    % m
 scene_data.Grid.num_sqrs_y = 40;    % m
 scene_data.Grid.line_width = 0.08;  % m
 scene_data.Grid.clr = [1 1 1];      % [R G B]
